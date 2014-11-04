fac032698b55abacd6a6f07654b7fcc0672a521e
Karen <karenpenglabs@gmail.com>
2014-11-04 00:24:49 -0500

    finally everything works

--- arduino/sketch_nov03b/sketch_nov03b.ino
+++ arduino/sketch_nov03b/sketch_nov03b.ino
@@ -9,45 +9,35 @@ void setup() {
   analogWrite(3,500);
   analogWrite(6,500);
   // Wait until a Serial Monitor is connected.
-  while (!Serial);
+//  while (!Serial);
   // launch the echo.js script asynchronously:
   nodejs.runShellCommandAsynchronously("node /mnt/sda1/yun/index.js");
   Serial.println("Started process");
 }
 void loop() {
-  // pass any bytes that come in from the serial port
-  // to the running node process:
-  if (Serial.available()) {
-    if (nodejs.running()) {
-      //nodejs.write(Serial.read());
+  while (nodejs.available()) {
+//    Serial.println(nodejs.read());
+    int currentPin = 0;
+    int inByte = nodejs.read();
+    switch(inByte){
+    case'x':
+      Serial.println('x');
+      currentPin = 3;
+      break;
+    case 'y':
+      Serial.println('y');
+      currentPin = 6;
+      break;
+    }
+    if(currentPin != 0){
+      int value = nodejs.parseInt();
+      Serial.println(currentPin);
+      Serial.println(value);
+      value = map(value, 0, 1000, 0, 255);
+      analogWrite(currentPin, value);
     }
   }
-  // pass any incoming bytes from the running node process
-  // to the serial port:
-  while (nodejs.available()) {
-    //Serial.write(nodejs.read());
-      int currentPin = 0;
-    // if we get a valid byte, read analog ins:
-    //if (Serial.available() > 0) {
-      // get incoming byte:
-      int inByte = nodejs.read();
-      switch(inByte){
-        case'x':
-         currentPin = 3;
-         break;
-        case 'y':
-         currentPin = 6;
-         break;
-      }
-      
-      if(currentPin != 0){
-        int value = Serial.parseInt();
-        Serial.println(currentPin);
-        Serial.println(value);
-        value = map(value, 0, 1000, 0, 255);
-        analogWrite(currentPin, value);
-      }
-   // }
-  }
 }
+
+
 
--- yun/index.js
+++ yun/index.js
@@ -1,33 +1,9 @@
 var WebSocket = require('faye-websocket');
-var ws = new WebSocket.Client('ws://128.122.6.143:4000');
-//var ws = new WebSocket.Client('ws://bluebinfinder.herokuapp.com');
-
-// var serialport = require("serialport");
-// var SerialPort = require("serialport").SerialPort;
-// var portName = process.argv[2];
-
-// console.log("opening serial port: " + portName);
-// var serialPort = new SerialPort(portName, {
-//   baudrate: 9600,
-//   parser: serialport.parsers.readline("\r\n")
-// });
-
-// //do handshake here
-// var contactEstablish = false;
-// serialPort.on('open', function () {
-//   console.log('open');
-//   serialPort.on('data', function (data) {
-//     console.log('from arduino: ' + typeof data + ' ' + data);
-//     if (data === 'A') {
-//       console.log("yeah");
-//       serialPort.write('A');
-//       contactEstablish = true;
-//     }
-//   });
-// });
+//var ws = new WebSocket.Client('ws://128.122.6.143:4000');
+var ws = new WebSocket.Client('ws://bluebinfinder.herokuapp.com');
 
 ws.on('open', function (event) {
-  //console.log('open');
+  console.log('O');
   ws.send('Hello, world!');
 });
 
@@ -42,19 +18,13 @@ ws.on('message', function (event) {
   if (!isNaN(test)) {
     //moving the arm to get the bin
     //format number/number
-    console.log("format number/number");
+    //console.log("format number/number");
     console.log('U' + subMessage[0] + ':' + subMessage[1]);
-    // if (contactEstablish) {
-    //   serialPort.write('U' + subMessage[0] + ':' + subMessage[1]);
-    // }
   } else {
     //moving the arm to set value
     //format: x/number
-    console.log("format axis/number");
+    //console.log("format axis/number");
     console.log(subMessage[0] + subMessage[1]);
-    // if (contactEstablish) {
-    //   serialPort.write(subMessage[0] + subMessage[1]);
-    // }
   }
 });
 
